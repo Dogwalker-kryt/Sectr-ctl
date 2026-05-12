d99aaec50b15478211c0d39b683581b1819ef9e7
Dogwalker-kryt <215691531+Dogwalker-kryt@users.noreply.github.com>
2026-05-12 08:07:41 +0200

    Delete DriveMgr_CLI/include directory

--- DriveMgr_CLI/include/dmgr_runtime_error.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "TermiosIO.h"
-
-/**
- * @brief A helper function to throw a std::runtime_error with a given error message and handles the terminal state poperly
- * @param msg The error message to include in the exception.
- */
-void dmgr_runtime_error(const std::string &msg);
