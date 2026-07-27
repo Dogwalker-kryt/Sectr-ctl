4b17225f6aa4ef3285863cf71db9bd31d85d3eac
Dogwalker-kryt <215691531+Dogwalker-kryt@users.noreply.github.com>
2026-07-27 17:01:57 +0200

    Delete DriveMgr_CLI/src directory

--- DriveMgr_CLI/src/utils/dmgr_runtime_error.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/utils/dmgr_runtime_error.hpp"
-
-void dmgr_runtime_error(const std::string &msg) {
-    TerminosIO::restoreTerminal();
-    std::cout << LEAVETERMINALSCREEN;
-    throw std::runtime_error("[ERROR] " + msg);
-}
